fdca9971fe0b002ce99af9627e0afe29a78c06bc
wesley-marcos <wesley.marcos@gec.inatel.br>
2023-10-08 19:11:16 -0300

    feat: Criação do script shell para envio do e-mail

---
+++ scripts/shell.sh
@@ -0,0 +1,13 @@
+# Início do trabalho
+echo "Trabalho de S107 iniciado"
+
+# Roda a notificação em paralelo
+notify-send "Notificação em paralelo rodando" &
+
+# Executa o job
+./job.sh
+
+# Fim do trabalho
+echo "Teste finalizado"
+sudo apt-get install mailutils
+echo "Enviando o e-mail do trabalho S107" | mail -s "Trabalho S107" lucasresendesrs@gmail.com
