18920b9f73cf42a3ec18fbf256adea867aa49b56
LucasSResende <lucasresendesrs@gmail.com>
2023-10-08 19:34:41 -0300

    fix: Implementando lista de e-mails nas notificações

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -10,4 +10,4 @@ notify-send "Notificação em paralelo rodando" &
 # Fim do trabalho
 echo "Teste finalizado"
 sudo apt-get install mailutils
-echo "Enviando o e-mail do trabalho S107" | mail -s "Trabalho S107" lucasresendesrs@gmail.com
+echo "Enviando o e-mail do trabalho S107" | mail -s "Trabalho S107" ${EMAIL}
